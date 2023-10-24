4592e890b2a1983e67d64bc712df3059e4fb92c9
aleksandrostroushko <a.ostroushko@zebrains.tearm>
2023-10-24 12:28:46 +0300

    Заменил икноку авторизации, убрал текст

--- local/templates/login/lang/ru/header.php
+++ local/templates/login/lang/ru/header.php
@@ -9,5 +9,4 @@ $MESS["BITRIX24_LANG_DE"] = "Немецкий";
 $MESS["BITRIX24_LANG_UA"] = "Украинский";
 $MESS["BITRIX24_LANG_LA"] = "Испанский";
 $MESS["BITRIX24_COPYRIGHT"] = "&copy; &laquo;Битрикс&raquo;, #CURRENT_YEAR#. Работает на 1С-Битрикс: Корпоративный портал.";
-$MESS["BITRIX24_COPYRIGHT_B24"] = "&copy; &laquo;Битрикс&raquo;, #CURRENT_YEAR#. Объединяем компанию с <a href=\"http://www.bitrix24.ru/\" target=\"_blank\">&laquo;1С-Битрикс24&raquo;</a>";
 ?>
